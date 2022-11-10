e9741254f972ef87366e063fdbaefd7746ee6f45
joseriosjose <jose_rios456@hotmail.com>
2022-11-10 11:54:38 -0600

    feat(mongo): add pokemon colection with schema

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,23 @@
-export class Pokemon {}
+/**referencia a la coleccion pokemon en mongo */
+
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Pokemon extends Document {
+  // id:string // mongo lo hace automatico
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  name: string;
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  no: number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
