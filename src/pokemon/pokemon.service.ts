2fe175200c298a031400ebb3f312690d1774285d
joseriosjose <jose_rios456@hotmail.com>
2022-11-10 14:15:21 -0600

    feat(pokemon):delete pokemon by no,name or mongo id

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -74,8 +74,9 @@ export class PokemonService {
     }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pokemon`;
+  async remove(id: string) {
+    const pokemon = await this.findOne(id);
+    await pokemon.deleteOne();
   }
 
   private handleExeptions(error: any) {
